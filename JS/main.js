43c5feebcb1a3d6541929154caf5eec1d6936551
Yash Nagla <naglayash@gmail.com>
2024-07-23 15:43:30 +0530

    Added Filter Function to Project Section

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    let $btn = $('.project-area .button-group button');
+    $btn.click(function(e){
+        $('.project-area .button-group buttons').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+            filter:selector
+        })
+        return false;
+    })
+})
